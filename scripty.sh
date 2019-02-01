1ac7346fed21bd9a909e02dac71ec9ed5ffab090
Durgasai2308 <42772258+Durgasai2308@users.noreply.github.com>
2019-02-01 15:51:06 -0500

    Update scripty.sh

--- scripty.sh
+++ scripty.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'This is example of script'
 echo 'That is running in a build step'
-echo "This is  the param $passed"
+echo "This is param $passed"
 NUM=$(($Firstnumber + $Secondnumber)) 
 echo $NUM
